d65c1582a090dbf35b67c6af5f3411f8453234d4
Autumn Miloser <91401881+fall-through@users.noreply.github.com>
2021-10-15 03:31:28 -0400

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,15 +1,9 @@
+
+//library
 #include <iostream>
 
 using namespace std;
 
-
-//create abstract class, makes class look less complex, makes it easier to read and for other dev to work on it
-class AbstractEmployee{
-    //Ask for promotion is a mehtod, void is return type
-    //virtual -- makes void AskforPromotion obligatory for any class that uses abstract employee
-    //have to provide implementation of askforpromotion in 
-    virtual void AskForPromotion()= 0;
-};
 
 //need to give return type, 
 //void means function wont return anything
@@ -20,6 +14,20 @@ int getMax(int num1, int num2);
 string getDayOfWeek(int dayNum);
 void whilelooops (int index);
 bool guessinggame(int secretNum, int guess);
+void ForLoopArrayEx();
+int ExponentFunction(int baseNum, int powNum);
+void twoDArrays_and_nestedForLoops();
+void PointerPractice();
+
+
+
+//create abstract class, makes class look less complex, makes it easier to read and for other dev to work on it
+class AbstractEmployee{
+    //Ask for promotion is a mehtod, void is return type
+    //virtual -- makes void AskforPromotion obligatory for any class that uses abstract employee
+    //have to provide implementation of askforpromotion in 
+    virtual void AskForPromotion()= 0;
+};
 
 //everything in class is private by default
 //Employee now can be used as a type (kinda like int string etc), we can name different objects using same type
@@ -349,3 +357,83 @@ bool guessinggame(int secretNum, int guess){
     cout << "you win";
     return 0;
 }
+
+void ForLoopArrayEx(){
+    //on first iteration will print nums[0], second: nums[1]
+ int nums[] = {1, 2, 5, 7, 3}
+    for(int i = 0; i < 5; i++){
+        cout << nums[i] << endl;
+        
+    }
+    
+}
+
+
+int ExponentFunction(int baseNum, int powNum){
+    int result = 1;
+    //powNum -- only want it to run that many times
+    for(int i = 0; i < powNum; i++){
+        //result will increase each time
+        //will be spitting out increasing power each time
+     result = result * baseNUm;   
+    }
+    
+    return result  
+}
+
+void twoDArrays_and_nestedForLoops(){
+    //first [] -- how many sets in array
+    //second [] -- how many elements in each set 
+ int numberGrid[3][2] = { 
+                        {1, 2}, //elem at position 0
+                        {3, 4}, //elem at position 1
+                        {5, 6}, //elem at position 2
+                      };
+    cout << numberGrid[0][1]; // will print 2
+    cout << numberGrid[2][0]; // will print 5
+    
+     for(int i = 0; i < 3; i++){
+     for(int j = 0; i < 2; j++){
+         cout << numberGrid[i][j]; // will print :
+         // 12
+         // 34
+         // 56
+     }
+         cout << endl;
+     
+}
+      
+    
+}
+//pointer === information :)
+// pointers are memory addresses
+// memory in computer -- ram (random access memory)
+//everytime computer runs its storing infor in memory (aka using ram)
+//values stored inside memory
+
+void PointerPractice(){
+    // pointers -- just working with just another type of data
+    //each value is inside containers
+    //yes, in our program 19 is tored in age -- but also stored in an address in physical memory of our computer
+    //use memory address for computer to access the variable
+    int age = 19;
+    int *pAge = &age; // storing memory address of age in pAge
+    double gpa = 2.7
+    double *pGpa = &gpa;
+    string name = "Cinnamon";
+    string *pName = &name;
+    
+    cout << pAge; // will print out pointer -- which will be memory address of age -- like 0x67466fed
+    
+    cout << &age; // will print out memory address where age variable is stored -- hexidecimal number -- if you went to that memory address you would find 19
+    //memory address is considered a pointer -- 0xfe3434 = pointer
+    
+    // Dereferencing a pointer , gives us value that was stored at memory address
+    cout << *pAge; // will give us 19
+    
+    cout << *&gpa; //gives us 2.7
+    cout << &*&gpa; //gives us memory address 
+    
+}
+
+
